fcebecea52535bb46f066f66fae8a1529391ec77
Your Name <peterwayne3293@gmail.com>
2024-01-20 16:12:49 -0500

    output-shaft encoder added

---
+++ teensy40_output_shaft/lib/Motor/rover_arm.h
@@ -0,0 +1,193 @@
+// META SETTINGS---------------------------------------------------------------
+#define USE_TEENSY 1
+#define IRQ_DEBUG_PRIORITY 10
+
+#ifndef BRUSHED_ARM
+#define BRUSHED_ARM 0
+#endif
+
+// #ifndef BRUSHLESS_ARM
+#define BRUSHLESS_ARM 1
+// #endif
+
+// CONFIGURATION---------------------------------------------------------------
+#define TEST_ENCODER 0
+#define SKIP_MASTERING 0
+#define MASTERING 0
+#define MASTERING_TEST 0
+#define TICK 1
+#define TEST_LOOP 1
+#define TEST_LIMIT_SWITCH 0
+
+#define PID_PERIOD_US 10000
+#define PID_DT (PID_PERIOD_US * 1e-6f)
+
+#define CYTRON_MAX_OUTPUT 99.0f
+#define SERVO_MAX_OUTPUT 300.0f
+
+// TEENSY PINS------------------------------------------------------------------
+#ifdef USE_TEENSY
+// Teensy 4.0 compatibility.
+#define SPI_HandleTypeDef void
+#define GPIO_TypeDef void
+#define GPIO_PinState int
+#define TIM_HandleTypeDef void
+#define GPIO_PIN_RESET 0
+#define GPIO_PIN_SET 1
+#endif
+
+#if BRUSHED_ARM == 1
+#define DELAY_FACTOR 1
+#define CS1 2
+#define CS2 3
+#define CS3 4
+#define DIR1 5
+#define PWM1 6
+#define DIR2 7
+#define PWM2 8
+#define DIR3 9
+#define PWM3 10
+
+#define NO1 22
+#define NO2 21
+#define NO3 20
+#define NO4 19
+#define NO5 18
+
+#define END_EFFECTOR_LASER NO5
+
+#define LIMIT_WRIST_PITCH_MAX NO1
+#define LIMIT_WRIST_PITCH_MIN NO2
+#define LIMIT_END_EFFECTOR_MAX NO3
+#define LIMIT_END_EFFECTOR_MIN NO4
+#endif
+
+#if BRUSHLESS_ARM == 1
+#define DELAY_FACTOR 1.5
+#define CS1 10
+#define CS2 9
+#define CS3 8
+#define PWM1 2
+#define PWM2 3
+#define PWM3 4
+
+#define NO1 22
+#define NO2 21
+#define NO3 20
+#define NO4 19
+#define NO5 18
+#define NO6 17
+
+#define ELBOW_BRAKE 14
+#define SHOULDER_BRAKE 15
+
+#define LIMIT_ELBOW_MAX NO1
+#define LIMIT_ELBOW_MIN NO2
+#define LIMIT_SHOULDER_MAX NO3
+#define LIMIT_SHOULDER_MIN NO4
+#define LIMIT_WAIST_MAX NO6
+#define LIMIT_WAIST_MIN NO5
+#endif
+
+// DEBUG SETTINGS---------------------------------------------------------------
+#define DEBUG_GDB_STUB 0
+#define DEBUG_PID 0
+#define DEBUG_ROVER_ARM_MOTOR 0
+#define DEBUG_PRINT_MOTOR 1
+#define DEBUG_ROVER_ARM_MOTOR_TICK 0
+
+// DEADBAND SETTINGS------------------------------------------------------------
+#define DEADBAND_CYTRON 0.8f
+#define DEADBAND_SERVO 18.0f
+#define IRQ_DEBUG IRQ_SOFTWARE
+#define USE_DMA 0
+
+// MOTOR SETTINGS---------------------------------------------------------------
+#if BRUSHED_ARM == 1
+#define TEST_WRIST_ROLL_CYTRON 1
+#define TEST_WRIST_PITCH_CYTRON 1
+#define TEST_END_EFFECTOR_CYTRON 1
+#endif
+
+#if BRUSHLESS_ARM == 1
+#define TEST_ELBOW_SERVO 0
+#define TEST_SHOULDER_SERVO 1
+#define TEST_WAIST_SERVO 0
+#endif
+
+// WRIST_ROLL_CYTRON
+#define REG_KP_WRIST_ROLL 0.15f
+#define REG_KI_WRIST_ROLL 0.1f
+#define REG_KD_WRIST_ROLL 0.2f
+// #define REG_KD_WRIST_ROLL 0.02f
+#define REG_KP_WRIST_ROLL_AGG 0.2f
+#define REG_KI_WRIST_ROLL_AGG 0.1f
+#define REG_KD_WRIST_ROLL_AGG 0.2f
+#define WRIST_ROLL_MIN_ANGLE -200.0f
+#define WRIST_ROLL_MAX_ANGLE 200.0f
+#define WRIST_ROLL_GEAR_RATIO 2.672222f
+#define WRIST_ROLL_ZERO_ANGLE 270.0f
+
+// WRIST_PITCH_CYTRON
+#define REG_KP_WRIST_PITCH 0.7f
+#define REG_KI_WRIST_PITCH 1.8f
+#define REG_KD_WRIST_PITCH 0
+#define REG_KP_WRIST_PITCH_AGG 1.0f
+#define REG_KI_WRIST_PITCH_AGG 2.3f
+#define REG_KD_WRIST_PITCH_AGG 0
+// #define WRIST_PITCH_MIN_ANGLE -75.0f
+#define WRIST_PITCH_MIN_ANGLE -35.0f
+// #define WRIST_PITCH_MAX_ANGLE 75.0f
+#define WRIST_PITCH_MAX_ANGLE 38.0f
+#define WRIST_PITCH_ZERO_ANGLE (323.0f + (-1.0f * 360.0f))
+// #define WRIST_PITCH_ZERO_ANGLE 60.0f
+#define WRIST_PITCH_GEAR_RATIO 1.0f
+
+// END_EFFECTOR_CYTRON
+#define REG_KP_END_EFFECTOR 0.4f
+#define REG_KI_END_EFFECTOR 0.0
+#define REG_KD_END_EFFECTOR 0.0
+#define END_EFFECTOR_MIN_ANGLE MIN_FLOAT
+#define END_EFFECTOR_MAX_ANGLE MAX_FLOAT
+#define END_EFFECTOR_ZERO_ANGLE MAX_FLOAT
+#define END_EFFECTOR_GEAR_RATIO 1.0f
+
+// ELBOW_SERVO
+#define REG_KP_ELBOW 1.2f
+#define REG_KI_ELBOW 0.3f
+#define REG_KD_ELBOW 0
+#define REG_KP_ELBOW_AGG REG_KP_ELBOW
+#define REG_KI_ELBOW_AGG REG_KI_ELBOW
+#define REG_KD_ELBOW_AGG REG_KD_ELBOW
+// #define REG_KP_ELBOW_AGG 0.8f
+// #define REG_KI_ELBOW_AGG 1.0f
+// #define REG_KD_ELBOW_AGG 0
+#define ELBOW_MIN_ANGLE -20.0f
+#define ELBOW_MAX_ANGLE 70.0f
+#define ELBOW_ZERO_ANGLE (240.0f + (0 * 360.0f))
+#define ELBOW_ANGLE_STRAIGHT -90.0f
+#define ELBOW_GEAR_RATIO 1.0f
+
+// SHOULDER_SERVO
+#define REG_KP_SHOULDER 2.0f
+#define REG_KI_SHOULDER 1.0f
+#define REG_KD_SHOULDER 1.8f
+#define REG_KP_SHOULDER_AGG REG_KP_SHOULDER * 1.2f
+#define REG_KI_SHOULDER_AGG REG_KI_SHOULDER * 1.2f
+#define REG_KD_SHOULDER_AGG REG_KD_SHOULDER
+#define SHOULDER_MIN_ANGLE -50.0f
+#define SHOULDER_MAX_ANGLE 70.0f
+#define SHOULDER_ZERO_ANGLE 85.0f + (-256.0f * 360.0f)
+#define SHOULDER_ANGLE_STRAIGHT 0
+
+// WAIST_SERVO
+#define REG_KP_WAIST 0.2f
+#define REG_KI_WAIST 1.5f
+#define REG_KD_WAIST 0
+#define REG_KP_WAIST_AGG 0.3f
+#define REG_KI_WAIST_AGG 2.0f
+#define REG_KD_WAIST_AGG 0
+#define WAIST_MIN_ANGLE -90.0f
+#define WAIST_MAX_ANGLE 90.0f
+#define WAIST_ZERO_ANGLE 151.0f + (-1.0f * 360.0f)
+#define WAIST_GEAR_RATIO 2.33f
---
+++ teensy40_output_shaft/src/main.cpp
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+
+#include "AMT22.h"
+#include <SPI.h>
+#include <EEPROM.h>
+#include "rover_arm.h"
+
+#define MIN_ADC_VALUE 0
+#define MAX_ADC_VALUE 4095
+
+float waist_angle = 0;
+float shoulder_angle = 0;
+float elbow_angle = 0;
+
+float map_float(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  double result = ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+
+  return result;
+}
+
+void setup()
+{
+  // Initiate SPI bus.
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV64);
+  pinMode(CS1, OUTPUT);
+}
+
+void loop()
+{
+  int16_t result_arr[2];
+  int error = getTurnCounterSPI(result_arr, CS1, 12);
+  if (error == -1)
+  {
+    Serial.println("Error reading SPI.");
+  }
+  else
+  {
+    float angle_raw = map_float((float)result_arr[0], MIN_ADC_VALUE, MAX_ADC_VALUE, 0, 359.99f);
+    float turns = result_arr[1];
+    waist_angle = angle_raw + 360 * turns;
+
+    Serial.printf("Angle: %d, Turns: %d ", result_arr[0], result_arr[1]);
+    Serial.printf("Angle_continous: %f", waist_angle);
+    Serial.println();
+  }
+}
