5a901f7fa093d03f3b33aecf928aa2024f7016ad
Jasmine Mao <jasminemao2004@gmail.com>
2024-02-03 13:48:08 -0500

    ports hard coded

--- transmitter/src/main.cpp
+++ transmitter/src/main.cpp
@@ -7,15 +7,18 @@ RF24 radio(7, 8); // CE, CSN
 
 const byte address[6] = "00001";
 
-void setup() {
-radio.begin();
-radio.openWritingPipe(address);
-radio.setPALevel(RF24_PA_HIGH);
-radio.stopListening();
+void setup()
+{
+    radio.begin();
+    radio.openWritingPipe(address);
+    radio.setPALevel(RF24_PA_HIGH);
+    radio.stopListening();
 }
 
-void loop() {
-const char text[] = "Test";
-radio.write(&text, sizeof(text));
-delay(1000);
+void loop()
+{
+    const char text[] = "Test 2";
+    radio.write(&text, sizeof(text));
+    Serial.println("sending");
+    delay(1000);
 }
