e7553b00811e02bc15e1f4272842c4bf77eb20f5
Jasmine Mao <jasminemao2004@gmail.com>
2024-02-04 11:40:25 -0500

    8 int output works

--- Receiver/src/main.cpp
+++ Receiver/src/main.cpp
@@ -2,8 +2,7 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8); // CE, CSN
-
+RF24 radio(7, 8); // CE, CSN 
 const byte address[6] = "00001";
 
 void setup()
@@ -19,8 +18,17 @@ void loop()
 {
   if (radio.available())
   {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
-  }
+    int text[8];
+    radio.read(&text, sizeof(text)); //NRF receives array of 8 integers
+
+    String output_int = "";
+
+    // Concatenate each element of the array to the string
+    for (int i = 0; i < sizeof(text) / sizeof(text[0]); ++i) {
+        output_int += String(text[i]) + " ";
+    }
+
+    // prints 8 integers: 4 for pH value, 4 for moisture value
+    Serial.println(output_int);
+    }
 }
