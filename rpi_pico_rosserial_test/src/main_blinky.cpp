4a59640fa331a64bd698cc281fb87b113de5b7bc
Your Name <peterwayne3293@gmail.com>
2024-02-26 16:59:35 -0500

    added rpi pico

---
+++ rpi_pico_rosserial_test/src/main_blinky.cpp
@@ -0,0 +1,19 @@
+// #include <Arduino.h>
+
+// #define LED_PIN 25
+
+// void setup() {
+//   // Initialize LED pin as an output.
+//   pinMode(LED_PIN, OUTPUT);
+//   // Initialize Serial communication at 115200 baud rate.
+//   Serial.begin(115200);
+// }
+
+// void loop() {
+//   digitalWrite(LED_PIN, HIGH); // Turn the LED on.
+//   Serial.println("Hello, World!111"); // Print "Hello, World!" to the Serial monitor.
+//   delay(100); // Wait for a second.
+
+//   digitalWrite(LED_PIN, LOW); // Turn the LED off.
+//   delay(100); // Wait for another second.
+// }
