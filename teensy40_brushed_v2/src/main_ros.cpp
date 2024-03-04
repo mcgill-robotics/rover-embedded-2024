71ecf4cf4637d23508988b4620def7117150ca3d
Your Name <peterwayne3293@gmail.com>
2024-03-04 16:02:53 -0500

    minor

--- teensy40_brushed_v2/src/main_ros.cpp
+++ teensy40_brushed_v2/src/main_ros.cpp
@@ -44,7 +44,7 @@ int tolerance = 0.5;
 volatile boolean is_homed = false;
 
 // FUNCTION DECLARATIONS
-//void brushed_board_homing();
+void brushed_board_homing();
 void brushed_board_ros_loop();
 
 void lim1ISR();
@@ -188,9 +188,9 @@ void setup()
     // Modelling
     current_rotation = Rotation2d::getRotationFromDeg(0);
 
-    //brushed_board_homing();
-    // while (!is_homed)
-    //     ;
+    // brushed_board_homing();
+    //  while (!is_homed)
+    //      ;
     loop_last_time = micros();
 }
 
@@ -277,15 +277,16 @@ void lim6ISR()
 }
 
 // HOMING SEQUENCE, go forward until limit switch is triggered
-// void brushed_board_homing()
-// {
-//     // Only do homing for linear joints
-//     if (!mot1._is_circular_joint)
-//     {
-//         mot1.set_direction(mot1._forward_dir);
-//         mot1._pwm_write_duty(40);
-//     }
-// }
+void brushed_board_homing()
+{
+    // Only do homing for linear joints
+    if (!mot1._is_circular_joint)
+    {
+        mot1.set_direction(mot1._forward_dir);
+        mot1._pwm_write_duty(40);
+    }
+}
+
 void brushed_board_ros_loop()
 {
     // delay(PID_PERIOD_MS);
@@ -296,16 +297,17 @@ void brushed_board_ros_loop()
         counter = current_time;
         mot1.closed_loop_control_tick();
         mot2.closed_loop_control_tick();
-        //mot3.closed_loop_control_tick();
+        // mot3.closed_loop_control_tick();
         loop_last_time = current_time;
     }
 
     // Feedback
-    arm_brushed_fb_msg.data_length = 3;
     arm_brushed_angle_ps[0] = mot1.get_current_angle_ps();
     arm_brushed_angle_ps[1] = mot2.get_current_angle_ps();
-    //arm_brushed_angle_ps[2] = mot3.get_current_angle_ps();
+    // arm_brushed_angle_ps[2] = mot3.get_current_angle_ps();
     arm_brushed_angle_ps[2] = 0.0;
+
+    arm_brushed_fb_msg.data_length = 3;
     arm_brushed_fb_msg.data = arm_brushed_angle_ps;
     arm_brushed_fb_pub.publish(&arm_brushed_fb_msg);
 
