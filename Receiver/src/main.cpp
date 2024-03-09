1c599ff6342426999b96f5d0a3af588cd5c302e6
Jasmine Mao <jasminemao2004@gmail.com>
2024-03-09 15:49:48 -0500

    stepper and dc added

--- Receiver/src/main.cpp
+++ Receiver/src/main.cpp
@@ -15,9 +15,14 @@ uint32_t lastTime = 0;
 //DC motors
 const int pwm0 = 2; // pwm 0-255 for speed
 const int dir0 = 3;  // dir high/low for direction
-
 const int pwm1 = 4; 
 const int dir1 = 5;  
+
+//stepper motor
+#define dirPin 9                //direction: high or low
+#define stepPin 10              //steps/revolutions: run stepsPerRev for loop for # of steps to take
+#define stepsPerRevolution 200  //number of steps per 1 rev
+                                //speed: frequency of pulses sent to step pin, higher freq = shorter delay
 
 
 void setup()  // setup for NRF
@@ -71,40 +76,33 @@ void brake1() {
 }
 
 
-// void loop()
-// {
-// #if DEBUG_PRINT == 1
-//   if (millis() - lastTime > 1000)
-//   {
-//     Serial.printf("RX Loop %lu\r\n", millis()); // Use %lu for millis() as it returns unsigned long
-//     lastTime = millis();
-//   }
-// #endif
-//   bool rx_flag = radio.available();
-//   if (rx_flag)
-//   {
-//     char rx_buffer[256];
-//     uint8_t len = radio.getDynamicPayloadSize(); // Get dynamic payload size
-//     if (len > 255)
-//       len = 255; // Ensure len does not exceed buffer size - 1 (for null terminator)
-//     radio.read(rx_buffer, len);
-//     rx_buffer[len] = '\0'; // Null-terminate the received string
-//     Serial.println("Received:");
-//     Serial.println(rx_buffer);
-//     Serial.printf("time: %lu\r\n", millis());
-//   }
-// }
+void step_setup() {
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+}
 
-void loop() {
-  forward0();
-  delay(1000);
 
-  brake0();
-  delay(1000);
 
-  reverse0();
-  delay(1000);
-  
-  brake0();
-  delay(1000);
+void loop()
+{
+#if DEBUG_PRINT == 1
+  if (millis() - lastTime > 1000)
+  {
+    Serial.printf("RX Loop %lu\r\n", millis()); // Use %lu for millis() as it returns unsigned long
+    lastTime = millis();
+  }
+#endif
+  bool rx_flag = radio.available();
+  if (rx_flag)
+  {
+    char rx_buffer[256];
+    uint8_t len = radio.getDynamicPayloadSize(); // Get dynamic payload size
+    if (len > 255)
+      len = 255; // Ensure len does not exceed buffer size - 1 (for null terminator)
+    radio.read(rx_buffer, len);
+    rx_buffer[len] = '\0'; // Null-terminate the received string
+    Serial.println("Received:");
+    Serial.println(rx_buffer);
+    Serial.printf("time: %lu\r\n", millis());
+  }
 }
