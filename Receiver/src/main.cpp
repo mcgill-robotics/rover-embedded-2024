2d3d0b5d0bdaab711cc05d7a273d3b074126a780
Jasmine Mao <jasminemao2004@gmail.com>
2024-04-06 13:12:13 -0400

    dc and stepper

--- Receiver/src/main.cpp
+++ Receiver/src/main.cpp
@@ -19,11 +19,9 @@ const int pwm1 = 4;
 const int dir1 = 5;  
 
 //stepper motor
-#define dirPin 9                //direction: high or low
-#define stepPin 10              //steps/revolutions: run stepsPerRev for loop for # of steps to take
-#define stepsPerRevolution 200  //number of steps per 1 rev
-                                //speed: frequency of pulses sent to step pin, higher freq = shorter delay
-
+#define dirPin 9                //dirPin: set high or low for CW/CCW
+#define stepPin 10              //stepPin: write HIGH then LOW for 1 step
+#define stepsPerRevolution 200  //number of steps per a full revolution
 
 void setup()  // setup for NRF
 {
@@ -42,22 +40,21 @@ void dc_setup() { // setup for DC motors 0 and 1
   pinMode(dir1,OUTPUT); 
 }
 
-int dc_speed0 = 200;  //set speed of DC motors 0 and 1
+ //sets the speed of DC motors
+int dc_speed0 = 200; 
 int dc_speed1 = 100;
 
 //control DC 0
-void dc_forward0() {  // turn CCW: set dir0 to HIGH
+void dc_forward0() {  // rotate CCW: set dir0 to HIGH
   digitalWrite(dir0, HIGH);
-  analogWrite(pwm0, dc_speed0);
+  analogWrite(pwm0, dc_speed0); 
 }
-
-void dc_reverse0() {  // turn CW: set dir0 to LOW
+void dc_reverse0() {  // rotate CW: set dir0 to LOW
   digitalWrite(dir0, LOW);
-  analogWrite(pwm0, dc_speed0);
+  analogWrite(pwm0, dc_speed0); 
 }
-
 void dc_brake0() {  // brake: set pwm0 to 0
-  analogWrite(pwm0, 0);
+  analogWrite(pwm0, 0); 
 }
 
 //control DC 1
@@ -65,34 +62,31 @@ void dc_forward1() {
   digitalWrite(dir1, HIGH);
   analogWrite(pwm1, dc_speed1);
 }
-
 void dc_reverse1() {  
   digitalWrite(dir1, LOW);
   analogWrite(pwm1, dc_speed1);
 }
-
 void dc_brake1() {  
   analogWrite(pwm1, 0);
 }
 
-
+//stepper motor
 void step_setup() { //setup for stepper motor
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
 }
-
 void step_cw() {  //turn CW: set dirPin HIGH
   digitalWrite(dirPin, HIGH);
 }
-
 void step_ccw() { //turn CCW: set dirPin LOW
   digitalWrite(dirPin, LOW);
 }
 
+//rotates the stepper 
 void step_rotate() {   
-  int revolutions = 0.25; //sets number of revolutions to rotate(0.25 for 90 degrees)
-  int delay_sec = 1000;   //controls speed: lower delay time is faster rotation
-  step_cw();            //set direction here
+  int revolutions = 0.25;   //revolutions sets number of revolutions to rotate(0.25 for 90 degrees)
+  int delay_sec = 1000;     //controls speed: lower delay time is faster rotation
+  step_cw();                //set direction: either step_cw() or step_ccw()
 
   for (int i = 0; i < revolutions * stepsPerRevolution; i++) {  //rotate Stepper for given speed/revolutions
     digitalWrite(stepPin, HIGH);
@@ -100,7 +94,6 @@ void step_rotate() {
     digitalWrite(stepPin, LOW);
     delayMicroseconds(delay_sec);
   }
-
 }
 
 
